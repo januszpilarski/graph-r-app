f3bd3a4321213bc725a2b26d99967de38973396a
janusz <pilarski.janusz@gmail.com>
2020-05-06 09:53:18 +0200

    Initial commit

---
+++ src/test/java/com/jpl/graphrapp/GraphRAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpl.graphrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphRAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
